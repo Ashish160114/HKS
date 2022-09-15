cf2b5835c6430a72535e55322c10e103a7060a33
ashish singh <ashish.singh@ioanyt.com>
2022-09-15 12:14:59 +0530

    extra code remove

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,19 +8,10 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  api: string = 'https://api.publicapis.org/entries'
-  dataList = []
   constructor(
     public http: HttpClient,
     private route: ActivatedRoute,
-  ) {
-    this.http.get(this.api).subscribe((res: any) => {
-      if (res) {
-        this.dataList = res.entries
-        console.log(this.dataList)
-      }
-    })
-  }
+  ) {  }
 
   ngOnInit(): void {
   }
